393c0bc77961f6f0639064c01e71722758aec034
Amlana-P <50160346+Amlana-P@users.noreply.github.com>
2020-10-07 17:54:22 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+
+let mouseCursor = document.querySelector(".cursor");
+let navLinks = document.querySelectorAll(".nav-links li");
+
+window.addEventListener('mousemove', cursor);
+
+function cursor(e){
+	mouseCursor.style.top = e.pageY + 'px'
+	mouseCursor.style.left = e.pageX + 'px'
+}
+
+Array.from(navLinks).forEach(link => {
+	link.addEventListener('mouseleave', () => {
+		mouseCursor.classList.remove('link-grow');
+		link.classList.remove('hovered-link');
+	})
+
+	link.addEventListener('mouseover', () => {
+		mouseCursor.classList.add('link-grow');
+		link.classList.add('hovered-link');
+	})
+});
